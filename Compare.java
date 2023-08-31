af0efc4909ff6955499603c2f535b411c8bb8438
Rohit j Shahare <130659863+info34rohit@users.noreply.github.com>
2023-08-31 12:21:04 +0530

    Add files via upload

---
+++ Compare.java
@@ -0,0 +1,14 @@
+class Compare
+{
+public static void main (String[]args)
+{
+int a = 4;
+int b = 5;
+if(b > a){
+ System.out.println("b is greater" );
+}
+else if(b < a){
+ System.out.println("a is greater");
+}
+}
+}
